09406eb7414b4b0c2ab1142e006395d62c6fea55
kalo83s <kalo_2002@abv.bg>
2022-01-09 16:52:01 +0100

    Init: initialised route for game.html

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* Pressing the 'PLAY' takes you to this page */
+router.get('/play', function(req, res) {
+  res.sendFile("game.html", {root: "./public"})
+});
+
+
 module.exports = router;
