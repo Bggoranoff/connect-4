5dbfb4949cb4e4d1616c24b2fc0d8a633209e07c
Bggoranoff <boris.goranov.bg@gmail.com>
2022-01-10 14:35:41 +0200

    Feat: static splash ejs

--- app.js
+++ app.js
@@ -5,5 +5,8 @@ const router = require("./routes");
 const port = process.argv[2];
 const app = express();
 
+app.use(express.static(__dirname + "/public"));
+
+app.set('view engine', 'ejs');
 app.use('/', router);
 http.createServer(app).listen(port);
