67001e967e2e02a13bf4d0bec1aed79f0f234fd1
kalo83s <kalo_2002@abv.bg>
2022-01-12 13:53:54 +0100

    Fix: coin resizable after fitting in place

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1,8 +1,6 @@
 [...document.getElementsByClassName("column")].forEach((element) => {
     element.addEventListener("click", (event) => {
-        let cell = element.children[0];
-        console.log(cell.getBoundingClientRect().bottom);
-        console.log(event.clientY);
+        let cell = element.children[5];
         let drop = document.createElement("div");
         let cloud = document.getElementById("cloudBox").getBoundingClientRect();
         drop.className = "drop";
@@ -10,12 +8,19 @@
         drop.style.width = cell.getBoundingClientRect().width + "px";
         drop.style.height = cell.getBoundingClientRect().height + "px";
         drop.style.left = cell.getBoundingClientRect().left + "px";
-        drop.style.top = cloud.bottom + window.scrollY - cloud.height/1.95 + "px";
+        drop.style.top = cloud.bottom + window.scrollY - cloud.height/2 + "px";
         drop.style.animation = "drop 4s linear";
-        drop.style.zIndex = -10;
+        drop.style.zIndex = -5;
         drop.style.setProperty("--b", (cell.getBoundingClientRect().bottom - (cloud.bottom + window.scrollY - cloud.height/2) - cell.getBoundingClientRect().height) + "px");
         drop.style.animationFillMode = "forwards";
         document.getElementById("gameScreen").appendChild(drop);
+        setTimeout(() => {
+            drop.remove();
+            cell.style.backgroundImage = "url('/images/LastDrop.png')";
+            cell.style.backgroundSize = "100% 100%";
+            cell.style.backgroundRepeat = "no-repeat";
+            cell.style.backgroundPosition = "center";
+        }, 4000);
     });
 });
 
