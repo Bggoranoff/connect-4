6ef80a037ca8be0df25a0b4229c9c63630a74d68
kalo83s <kalo_2002@abv.bg>
2022-01-14 13:37:45 +0100

    Fix: setup file restored

--- routes/index.js
+++ routes/index.js
@@ -26,6 +26,10 @@ router.get('/win', function(req, res) {
   res.render("winning.ejs", {});
 });
 
+router.get('/loss', function(req, res) {
+  res.render("losing.ejs", {});
+});
+
 router.get('/stats', function(req, res) {
   res.send(JSON.stringify(stats));
 });
